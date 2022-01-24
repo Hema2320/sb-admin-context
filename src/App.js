2b49da2bd20c5ee252d57c96057cda0fa0c0e546
Hema2320 <hemadharsh23@gmail.com>
2022-01-24 06:23:32 +0530

    Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar'
+import Dashboard from './components/Dashboard';
+import Allstudents from './components/Allstudents';
+import Addstudents from './components/Addstudents';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Editstudents from './components/Editstudents';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let data = {
+    earning:"1,00,000",
+    annual:"2,50,00000",
+    task:20,
+    pending:26,
+  }
+  let [students,setStudents] = useState([
+    {
+      name:"Hema",
+      email:"hema2320@gmail.com",
+      mobile:"909084561",
+      class:"B30WD"
+    },
+    {
+      name:"VenkateshIyer",
+      email:"venky2512@gmail.com",
+      mobile:"834512900",
+      class:"B30WD"
+    },
+    {
+      name:"Rabada",
+      email:"rabada@gmail.com",
+      mobile:"959493921",
+      class:"B30WD"
+    },
+    {
+      name:"Anrich",
+      email:"anrich@gmail.com",
+      mobile:"12345678",
+      class:"B30WD",
+    }
+]);
+
+  return <>
+    <BrowserRouter>
+        <div style={{display:"grid",gridTemplateColumns:"20% 80%"}}>
+            <div >
+                <Sidebar/>
+            </div>
+            <div>
+                <Routes>
+                      <Route path='/dashboard' element={<Dashboard data={data}/>}/>
+                      <Route path ='/all-students' element={<Allstudents data={{students,setStudents}}/>}/>
+                      <Route path = '/add-student' element={<Addstudents data={{students,setStudents}}/>}/>
+                      <Route path='/edit-student/:id' element={<Editstudents data={{students,setStudents}}/>}/>
+                      <Route path = '/' element={<Dashboard data={data}/>}/>
+                </Routes>
+            </div>
+        </div>
+    </BrowserRouter>
+  </>
 }
 
 export default App;
